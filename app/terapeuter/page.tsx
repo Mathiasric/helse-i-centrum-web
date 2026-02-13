473bcc92b8239f979ee5cae2e4ebbc288b189ce9
Mathias Richardsen <mathias@example.com>
2026-02-13 11:51:55 +0100

    fix

--- app/terapeuter/page.tsx
+++ app/terapeuter/page.tsx
@@ -64,8 +64,8 @@ export default function TherapistsPage() {
               className={
                 [
                   "w-full min-w-0 lg:col-span-2",
-                  therapists.length === 5 && index === 3 ? "lg:col-start-2 lg:mr-9" : "",
-                  therapists.length === 5 && index === 4 ? "lg:col-start-4 lg:ml-9" : "",
+                  therapists.length === 5 && index === 3 ? "lg:col-start-2 lg:mr-[2.5rem]" : "",
+                  therapists.length === 5 && index === 4 ? "lg:col-start-4 lg:ml-[2.5rem]" : "",
                 ]
                   .filter(Boolean)
                   .join(" ")
